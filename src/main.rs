a29412fa65cd2c3e797f62b8915fe9c981025719
p0d4r14n <p0d4r14n@protonmail.com>
2023-11-10 11:00:53 +0100

    optimize error handling on CLI command

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,38 @@ struct Args {
 }
 
 fn main() {
+    let available_years = ["2015", "2021", "2022"];
     let args = Args::parse();
+
+    if args.year.len() != 4 {
+        panic!("Invalid year: {} - Year must have 4 digits", args.year);
+    }
+
+    if available_years.contains(&args.year.as_str()) == false {
+        panic!("Invalid year: {} - Available years are {:?}", args.year, available_years);
+        
+    }
+
+    let day = if args.day.len() == 2 {
+        "0".to_string() + &args.day
+    } else if args.day.len() == 3 {
+        args.day.to_string()
+    } else {
+        if !args.day.contains("a") && !args.day.contains("b") {
+            panic!("Invalid day: {} - Day must have a or b as suffix, i.e. 01a", args.day);
+        }
+        panic!("Invalid day: {} - Day must have 1 or 2 digits and end with a or b, i.e. 01a", args.day);
+    };
+
     match args.year.as_str() {
         "2015" => {
-            println!("{}", year2015::solution(&args.day));
+            println!("{}", year2015::solution(&day));
         },
         "2021" => {
-            println!("{}", year2021::solution(&args.day));
+            println!("{}", year2021::solution(&day));
         },
         "2022" => {
-            println!("{}", year2022::solution(&args.day));
+            println!("{}", year2022::solution(&day));
         },
         _ => {}
     }
